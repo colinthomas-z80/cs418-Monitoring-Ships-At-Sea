ac87a6d3364c267c48cf992670b485c60ee8a933
tokanada <davidt.3200@gmail.com>
2021-04-27 17:43:31 -0400

    Add Unit Tests for create_database.py

--- unittests.py
+++ unittests.py
@@ -1,2 +1,28 @@
-import unittests
+import unittest as test
+import create_database
+import mysql.connector
 
+class ais_unit_tests(test.TestCase):
+
+    # Tests unit tester
+    def test_if_true(self):
+        self.assertTrue(True, "Pass")
+        
+    def test_if_false(self):
+        self.assertFalse(False, "Pass")
+        
+    # Unit Tests for create_database.py
+    def test_create_database(self):
+        create_database
+        error = False
+        client = mysql.connector.connect(host = 'localhost', user = 'iqrasuhail', passwd = 'marviabroforlife')
+        cursor = client.cursor()
+        try:
+            cursor.execute("USE AISDraft")
+        except:
+            error = True
+        self.assertFalse(error)
+    
+
+if __name__ == '__main__':
+    test.main()
