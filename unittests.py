1b77169e3af5cf7140fd21f263e2d6f8c7d9a65f
tokanada <davidt.3200@gmail.com>
2021-04-29 02:43:09 -0400

    Add unit tests for insert_message_batch() Add sample_test.json

--- unittests.py
+++ unittests.py
@@ -34,6 +34,8 @@ class ais_unit_tests(test.TestCase):
         self.assertTrue(error)
 
     # Unit Tests for tmb_dao.py
+
+    # Test for insert.msg()
     def test_insert_message_pass(self):
         error = False
         ex = '{"Timestamp":"2020-11-18T00:00:00.000Z","Class":"Class A","MMSI":244265000,"MsgType":"position_report", \
@@ -44,6 +46,25 @@ class ais_unit_tests(test.TestCase):
             error = True
         self.assertFalse(error)
 
+    def test_insert_message_fail(self):
+        error = False
+        ex = '{"Timstamp":"2020-11-18T00:00:00.000Z","Class":"Class A","MMSI":244265000,"MsgType":"position_report", \
+                     "Position":{"type":"Point","coordinates":[55.522592,15.068637]},"Status":"Under way using engine","RoT":2.2,"SoG":14.8,"CoG":62,"Heading":61}'
+        try:
+            tmb_dao.tmb_dao().insert_msg(ex, 0)
+        except KeyError:
+            error = True
+        self.assertTrue(error)
+
+    # Test for insert_message_batch()
+    def test_insert_message_batch_pass(self):
+        error = False
+        try:
+            tmb_dao.tmb_dao().insert_message_batch("scripts/sample_test.json")
+        except Exception as e:
+            print(e)
+            error = True
+        self.assertFalse(error)
 
 if __name__ == '__main__':
     test.main()
