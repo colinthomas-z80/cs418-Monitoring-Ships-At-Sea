417d2b895701550b8275126b7a3bd96e25aab967
Colin Thomas <cthomas0687@gmail.com>
2021-04-29 08:56:21 -0400

    finish read position by mmsi query

--- tmb_extended.py
+++ tmb_extended.py
@@ -6,11 +6,14 @@ class tmb_extended:
 
     # given an mmsi, go through the most recent AIS_MESSAGEs until you find a position report. Get the position.
     def read_position_by_mmsi(self, mmsi):
-        query = ("SELECT Latitude, Longitude FROM AISDraft.POSITION_REPORT  \
-                  WHERE AISMessage_Id IN                                    \
-                  (SELECT Id FROM AISDraft.AIS_MESSAGE WHERE MMSI = {0} ORDER BY Timestamp) LIMIT 1;").format(mmsi)
-        messages = SQL_runner().run(query)
-        print(messages)
+        query2 = "SELECT MMSI, Latitude, Longitude FROM AISDraft.POSITION_REPORT, AISDraft.AIS_MESSAGE \
+                  WHERE POSITION_REPORT.AISMessage_Id = AIS_MESSAGE.Id  \
+                  AND AIS_MESSAGE.MMSI = {0} \
+                  ORDER BY Timestamp \
+                  LIMIT 1;".format(mmsi) 
+                  
+        result = SQL_runner().run(query2)
+        return result
     
 
 tmb_extended().read_position_by_mmsi(992111929)
