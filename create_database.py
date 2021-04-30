034354828b702a33d8832cfd9b6957ba0f87d9be
Colin Thomas <cthomas0687@gmail.com>
2021-04-30 09:58:46 -0400

    db creation inserts map view, vessel and port

--- create_database.py
+++ create_database.py
@@ -21,4 +21,26 @@ except:
 
 print("OK")
 
+print("INSERTING VALUES....")
 
+vessels = open("scripts/VESSEL_VALUES.sql").read()
+map_views = open("scripts/MAP_VIEW_VALUES2.sql").read()
+ports = open("scripts/PORT_VALUES.sql").read()
+
+print("VESSEL....")
+try:
+    err = SQL_runner().run("INSERT INTO AISDraft.VESSEL VALUES " + vessels)
+except:
+    print("Error inserting vessel values")
+
+print("MAP VIEW....")
+try:
+    err = SQL_runner().run("INSERT INTO AISDraft.MAP_VIEW VALUES " + map_views)
+except:
+    print("Error inserting map view values")
+
+print("PORT....")
+try:
+    err = SQL_runner().run("INSERT INTO AISDraft.PORT VALUES " + ports)
+except:
+    print("Error inserting port values")
